8804489d0316d84a925c982bca9bb929f3c2fe52
Adrián Doña Mateo <drnaia100@gmail.com>
2021-02-11 14:19:26 +0000

    Adapt 2nd iso theorem to mathlib

--- src/jordan-holder/quotient_group.lean
+++ src/jordan-holder/quotient_group.lean
@@ -18,34 +18,23 @@ lemma equiv_quotient_of_eq {M N : subgroup G} [M.normal] [N.normal] :
   map_mul' := λ x y, by rw map_mul,
 }
 
-section snd_isomorphism_theorem
-
-def φ (H N : subgroup G) [N.normal] : H →* quotient (N.comap (H.prod_normal N).subtype) :=
-(mk' $ N.comap (H.prod_normal N).subtype).comp (inclusion le_prod_normal_left)
-
-lemma ker_φ {H N : subgroup G} [N.normal] : (φ H N).ker = (H ⊓ N).comap H.subtype :=
-ext $ λ x, 
-  ⟨λ h, by { simp, use x.2, dsimp [φ] at h, rw [← comap_ker, ker_mk, mem_comap] at h, simpa using h }, 
-  λ h, by { dsimp [φ], rw [← comap_ker, ker_mk], simp at h, simpa using h.2 }⟩
-
-lemma φ_surjective {H N : subgroup G} [N.normal] : function.surjective (φ H N) :=
-λ x, begin
-  rcases quot.exists_rep x with ⟨⟨w, h, hh, n, hn, rfl⟩, rfl⟩,
-  use [h, hh], apply quotient.eq.mpr, change h⁻¹ * (h * n) ∈ N,
-  rwa [← mul_assoc, inv_mul_self, one_mul],
-end
-
 /- The second isomorphism theorem: given two subgroups `H` and `N` of a group `G`, where `N`
 is normal, defines an isomorphism between `H/(H ∩ N)` and `(HN)/N`. -/
 noncomputable def quotient_inf_equiv_prod_normal_quotient (H N : subgroup G) [N.normal] :
-  quotient ((H ⊓ N).comap H.subtype) ≃* quotient (N.comap (H.prod_normal N).subtype) :=
-mul_equiv.trans (equiv_quotient_of_eq ker_φ.symm)
-  (quotient_ker_equiv_of_surjective (φ H N) φ_surjective)
-
-
-
-#check Prop
-
-end snd_isomorphism_theorem
+  quotient ((H ⊓ N).comap H.subtype) ≃* quotient (N.comap (H ⊔ N).subtype) :=
+let φ : H →* quotient (N.comap (H ⊔ N).subtype) :=
+  (mk' $ N.comap (H ⊔ N).subtype).comp (inclusion le_sup_left) in
+have φ_surjective : function.surjective φ := λ x, x.induction_on' $
+  begin
+    rintro ⟨y, (hy : y ∈ ↑(H ⊔ N))⟩, rw mul_normal H N at hy,
+    rcases hy with ⟨h, n, hh, hn, rfl⟩,
+    use [h, hh], apply quotient.eq.mpr, change h⁻¹ * (h * n) ∈ N,
+    rwa [← mul_assoc, inv_mul_self, one_mul],
+  end,
+suffices h : (H ⊓ N).comap H.subtype = φ.ker,
+  from mul_equiv.trans (equiv_quotient_of_eq h) (quotient_ker_equiv_of_surjective φ φ_surjective),
+ext $ λ x,
+  ⟨λ h, by { dsimp [φ], rw [← comap_ker, ker_mk], simp at h, simpa using h.2 },
+  λ h, by { simp, use x.2, dsimp [φ] at h, rw [← comap_ker, ker_mk, mem_comap] at h, simpa using h }⟩
 
 end quotient_group
